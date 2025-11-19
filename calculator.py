ef3ffe7f4023cb6dc4e0d58762d5ea2d5da535c6
thekrishsaluja <programming.kris9801@gmail.com>
2025-11-18 22:18:19 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -16,15 +16,18 @@ def add(a, b):
 def subtract(a,b):
     return a - b
 
-def multiply(a,b):
-    return a * b
-
-def divide(a,b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
-
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError
     return math.log(b, a)
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def exp(a, b):
+    return a ** b
